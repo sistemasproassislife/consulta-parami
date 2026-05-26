f588714f6591e038142cb0369a47e147494d7c6d
Sistemas Proassislife <sistemas.proassismed@gmail.com>
2026-05-26 13:10:34 -0500

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "Consulta ParaMi",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="es">
+      <body>{children}</body>
+    </html>
+  );
+}
